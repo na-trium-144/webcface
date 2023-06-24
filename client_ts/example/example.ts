b428e215ec34b897c6f62107411c3a0f1195d2b4
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-06-24 19:45:11 +0900

    npmパッケージ作成

---
+++ client_ts/example/example.ts
@@ -0,0 +1,4 @@
+import { Client } from "../src/index";
+
+const c = new Client("example_ts");
+const v = c.value();
---
+++ client_ts/src/client.ts
@@ -0,0 +1,17 @@
+export class Client {
+  constructor(name: string, host = "", port = 80) {
+    console.log("name");
+  }
+  send() {
+    console.log("hoge");
+  }
+  value() {
+    console.log("hoge");
+  }
+  text() {
+    console.log("hoge");
+  }
+  func() {
+    console.log("hoge");
+  }
+}
