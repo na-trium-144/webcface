1d0bee37954cc8715ec200aac50b65a27f1b6259
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-08-02 15:14:09 +0900

    valueとtextにスタイルを適用

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,6 +1,6 @@
 import { useState, useEffect, useRef } from "react";
 import { Client } from "webcface";
-import './index.css';
+import "./index.css";
 
 function App() {
   const cli = useRef<Client | null>(null);
@@ -38,24 +38,28 @@ function App() {
   }, []);
 
   return (
-    <>
-      <h3>values</h3>
-      <ul>
-        {values.map((v) => (
-          <li key={v.name}>
-            {v.name} = {v.value}
-          </li>
-        ))}
-      </ul>
-      <h3>texts</h3>
-      <ul>
-        {texts.map((v) => (
-          <li key={v.name}>
-            {v.name} = {v.value}
-          </li>
-        ))}
-      </ul>
-    </>
+    <div className="p-1">
+      <h3 className="text-lg">values</h3>
+      {values.map((v) => (
+        <div
+          key={v.name}
+          className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 w-60 h-60"
+        >
+          <h6 className="flex-none mx-auto font-semibold">{v.name}</h6>
+          <div className="flex-auto">{v.value}</div>
+        </div>
+      ))}
+      <h3 className="text-lg">texts</h3>
+      {texts.map((v) => (
+        <div
+          key={v.name}
+          className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 w-60 h-24"
+        >
+          <h6 className="flex-none mx-auto font-semibold">{v.name}</h6>
+          <div className="flex-auto">{v.value}</div>
+        </div>
+      ))}
+    </div>
   );
 }
 
