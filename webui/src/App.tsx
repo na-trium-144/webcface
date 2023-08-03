5e7b19c88e6c61c63e480995806b8933156a6fee
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-08-03 15:46:04 +0900

    react-grid-layoutついか

--- webui/src/App.tsx
+++ webui/src/App.tsx
@@ -1,6 +1,11 @@
 import { useState, useEffect, useRef } from "react";
 import { Client } from "webcface";
 import "./index.css";
+import "react-grid-layout/css/styles.css";
+import "react-resizable/css/styles.css";
+import { Responsive, WidthProvider } from "react-grid-layout";
+const ResponsiveGridLayout = WidthProvider(Responsive);
+import { getFromLS, saveToLS } from "./libs/ls";
 
 function App() {
   const cli = useRef<Client | null>(null);
@@ -9,6 +14,13 @@ function App() {
   const [funcs, setFuncs] = useState<
     { from: string; name: string; returnType: number; argsType: number[] }[]
   >([]);
+  const [layouts, setLayouts] = useState<object>({});
+  const [layoutsLoadDone, setLayoutsLoadDone] = useState<boolean>(false);
+  useEffect(() => {
+    setLayouts(getFromLS("layouts") || {});
+    setLayoutsLoadDone(true);
+  }, []);
+
   useEffect(() => {
     cli.current = new Client("a", "127.0.0.1", 80);
     setInterval(() => {
@@ -54,64 +66,93 @@ function App() {
     }, 100);
   }, []);
 
+  const onLayoutChange = (layout, layouts: object) => {
+    if (layoutsLoadDone) {
+      saveToLS("layouts", layouts);
+      setLayouts(layouts);
+    }
+  };
+
   return (
-    <div className="p-1">
-      <h3 className="text-lg">values</h3>
-      {values.map((v) => (
+    <div className="p-1 h-screen">
+      <ResponsiveGridLayout
+        className="layout h-full overflow-hidden"
+        layouts={layouts}
+        breakpoints={{ xxl: 1536, xl: 1280, lg: 1024, md: 768, sm: 640, xs: 0 }}
+        cols={{ xx: 15, xl: 13, lg: 10, md: 7, sm: 6, xs: 2 }}
+        rowHeight={100}
+        onLayoutChange={onLayoutChange}
+        allowOverlap
+        compactType={null}
+        autoSize={false}
+      >
+        {values.map((v) => (
+          <div
+            key={v.name}
+            data-grid={{ w: 2, h: 2, x: 0, y: 0, minW: 2, minH: 2 }}
+            className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 bg-white"
+          >
+            <h6 className="flex-none mx-auto font-semibold">{v.name}</h6>
+            <div className="flex-auto">{v.value}</div>
+          </div>
+        ))}
+
         <div
-          key={v.name}
-          className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 w-60 h-60"
+          key={"texts"}
+          data-grid={{ w: 4, h: 2, x: 0, y: 0, minW: 2, minH: 1 }}
+          className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 bg-white"
         >
-          <h6 className="flex-none mx-auto font-semibold">{v.name}</h6>
-          <div className="flex-auto">{v.value}</div>
+          <h6 className="flex-none mx-auto font-semibold">Text Status</h6>
+          <div className="flex-auto">
+            <ul className="list-none">
+              {texts.map((v) => (
+                <li key={v.name}>
+                  {v.name} = {v.value}
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      ))}
-      <h3 className="text-lg">texts</h3>
-      {texts.map((v) => (
         <div
-          key={v.name}
-          className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 w-60 h-24"
+          key="funcs"
+          data-grid={{ w: 6, h: 2, x: 0, y: 0, minW: 2, minH: 1 }}
+          className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 bg-white"
         >
-          <h6 className="flex-none mx-auto font-semibold">{v.name}</h6>
-          <div className="flex-auto">{v.value}</div>
+          <h6 className="flex-none mx-auto font-semibold">Funcs</h6>
+          <div className="flex-auto overflow-y-auto">
+            <ul className="list-none">
+              {funcs.map((v) => (
+                <li key={`${v.from}::${v.name}`} className="">
+                  <span className="">
+                    {v.from}::{v.name}
+                  </span>
+                  <span className="pl-1">(</span>
+                  {v.argsType.map((a, i) => (
+                    <>
+                      {i > 0 && <span className="pl-1 pr-1">,</span>}
+                      <input
+                        className="border-0 border-b outline-0 focus:border-b-2 border-neutral-200 hover:border-neutral-500 focus:border-black px-1"
+                        key={i}
+                        size="10"
+                      />
+                    </>
+                  ))}
+                  <span className="pr-2">)</span>
+                  <button
+                    className="rounded-full border border-green-300 shadow-md bg-green-100 px-2 my-1"
+                    onClick={() =>
+                      // todo: 引数
+                      void cli.current.subject(v.from).func(v.name).run()
+                    }
+                  >
+                    Run
+                  </button>
+                </li>
+              ))}
+            </ul>
+          </div>
         </div>
-      ))}
-      <h3 className="text-lg">funcs</h3>
-      <div className="flex flex-col rounded-md border border-neutral-200 shadow-md p-1 w-auto h-40">
-        <h6 className="flex-none mx-auto font-semibold">Funcs</h6>
-        <div className="flex-auto overflow-y-auto">
-          <ul className="list-none">
-            {funcs.map((v) => (
-              <li key={`${v.from}::${v.name}`} className="">
-                <span className="">
-                  {v.from}::{v.name}
-                </span>
-                <span className="pl-1">(</span>
-                {v.argsType.map((a, i) => (
-                  <>
-                    {i > 0 && <span className="pl-1 pr-1">,</span>}
-                    <input
-                      className="border-0 border-b outline-0 focus:border-b-2 border-neutral-200 hover:border-neutral-500 focus:border-black px-1"
-                      key={i}
-                      size="10"
-                    />
-                  </>
-                ))}
-                <span className="pr-2">)</span>
-                <button
-                  className="rounded-full border border-green-300 shadow-md bg-green-100 px-2 my-1"
-                  onClick={() =>
-                    // todo: 引数
-                    void cli.current.subject(v.from).func(v.name).run()
-                  }
-                >
-                  Run
-                </button>
-              </li>
-            ))}
-          </ul>
-        </div>
-      </div>
+      </ResponsiveGridLayout>
     </div>
   );
 }
---
+++ webui/src/libs/ls.ts
@@ -0,0 +1,27 @@
+const lsKey = "webcface-webui";
+
+function getLS() {
+  let ls = {};
+  if (global != undefined && global.localStorage) {
+    try {
+      ls = (JSON.parse(global.localStorage.getItem(lsKey)) as object) || {};
+    } catch (e) {
+      /*Ignore*/
+    }
+  }
+  return ls;
+}
+export function getFromLS(key: string) {
+  const ls = getLS();
+  // console.log("load", ls[key]);
+  return (ls[key] as object) || null;
+}
+
+export function saveToLS(key: string, value: object) {
+  if (global != undefined && global.localStorage) {
+    const ls = getLS();
+    ls[key] = value;
+    global.localStorage.setItem(lsKey, JSON.stringify(ls));
+    // console.log("save", ls)
+  }
+}
