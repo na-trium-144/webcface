c0543e7a80a63d9de2618c4f5d130c7458feb867
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-09-20 23:29:36 +0900

    jsを別リポジトリに移動

--- webui/src/libs/cardKey.ts
+++
@@ -1,8 +0,0 @@
-// import * as cardKey で使う
-export const value = (member: string, field: string) =>
-  `${member}:value:${field}`;
-export const view = (member: string, field: string) =>
-  `${member}:view:${field}`;
-export const text = (member: string) => `${member}:text`;
-export const func = (member: string) => `${member}:func`;
-export const log = (member: string) => `${member}:log`;
--- webui/src/libs/version.ts
+++
@@ -1,3 +0,0 @@
-import * as config from "../../package.json";
-const config_ = config;
-export default config_.version;
