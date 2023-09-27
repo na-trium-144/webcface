ddbe4de969c80002a31e25996f2571855b8ec280
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2023-09-27 19:35:54 +0900

    version修正

--- src/include/webcface/common/def.h
+++ src/include/webcface/common/def.h
@@ -1,12 +1,14 @@
 #pragma once
 #define WEBCFACE_STR(v) #v
 #define WEBCFACE_DEFAULT_PORT 7530
-#define WEBCFACE_DEFAULT_PORT_S WEBCFACE_STR(WEBCFACE_DEFAULT_PORT)
+#define WEBCFACE_DEFAULT_PORT_SI(port) WEBCFACE_STR(port)
+#define WEBCFACE_DEFAULT_PORT_S WEBCFACE_DEFAULT_PORT_SI(WEBCFACE_DEFAULT_PORT)
 #define WEBCFACE_SERVER_NAME "webcface"
 #define WEBCFACE_VERSION_MAJOR 1
 #define WEBCFACE_VERSION_MINOR 0
 #define WEBCFACE_VERSION_REVISION 0
+#define WEBCFACE_VERSION_I(maj, min, rev)                                      \
+    WEBCFACE_STR(maj) "." WEBCFACE_STR(min) "." WEBCFACE_STR(rev)
 #define WEBCFACE_VERSION                                                       \
-    WEBCFACE_STR(WEBCFACE_VERSION_MAJOR)                                       \
-    "." WEBCFACE_STR(WEBCFACE_VERSION_MINOR) "." WEBCFACE_STR(                 \
-        WEBCFACE_VERSION_REVISION)
+    WEBCFACE_VERSION_I(WEBCFACE_VERSION_MAJOR, WEBCFACE_VERSION_MINOR,         \
+                       WEBCFACE_VERSION_REVISION)
