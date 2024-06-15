31a7f797eca9f7b44215791d7ac50f2de1cd6dfa
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-06-15 20:35:56 +0900

    crowを別ライブラリに隔離

---
+++ src/server/internal/server_ws.h
@@ -0,0 +1,31 @@
+#pragma once
+#include <functional>
+
+namespace webcfaceServerInternal {
+using wsConnPtr = void *;
+
+using LoggerCallback =
+    std::function<void(const char *, unsigned long long, int)>;
+using OpenCallback = std::function<void(void *, const char *)>;
+using CloseCallback = std::function<void(void *, const char *)>;
+using MessageCallback =
+    std::function<void(void *, const char *, unsigned long long)>;
+using StartCallback = std::function<void()>;
+
+class AppWrapper {
+    void *app = nullptr;
+    void *exception_str = nullptr;
+
+    void setException(const char *what) noexcept;
+  public:
+    AppWrapper(const LoggerCallback &callback, const char *static_dir, int port,
+               const char *unix_path, const OpenCallback &on_open,
+               const CloseCallback &on_close, const MessageCallback &on_message,
+               const StartCallback &on_start) noexcept;
+    ~AppWrapper() noexcept;
+    static void send(wsConnPtr conn, const char *msg, unsigned long long size) noexcept;
+    void stop() noexcept;
+    void run() noexcept;
+    const char *exception() noexcept;
+};
+} // namespace webcfaceServerInternal
