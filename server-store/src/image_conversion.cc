2019f4acdd6ebdb0c947fca0b0260651be32ebd8
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-07-16 03:25:38 +0900

    serverビルドしないならmagickppも不要

--- server-store/src/image_conversion.cc
+++ server-store/src/image_conversion.cc
@@ -3,13 +3,18 @@
 #include "webcface/server/server.h"
 #include "webcface/message/message.h"
 #include "webcface/common/def.h"
+
+#ifdef WEBCFACE_SERVER
 #include <Magick++.h>
+#endif
 
 WEBCFACE_NS_BEGIN
 namespace server {
 
 // メインのスレッドで呼ばれるべき (server側でチェック)
 void initMagick() {
+#ifndef WEBCFACE_SERVER
+#else
     static std::mutex m;
     std::lock_guard lock(m);
     static bool initialized = false;
@@ -17,9 +22,10 @@ void initMagick() {
         Magick::InitializeMagick(nullptr);
         initialized = true;
     }
+#endif
 }
 
-static std::string magickColorMap(ImageColorMode mode) {
+[[maybe_unused]] static std::string magickColorMap(ImageColorMode mode) {
     switch (mode) {
     case ImageColorMode::gray:
         return "K";
@@ -41,8 +47,12 @@ static std::string magickColorMap(ImageColorMode mode) {
  * cd.image[field]が更新されるかリクエストが更新されたときに変換を行う。
  *
  */
-void MemberData::imageConvertThreadMain(const SharedString &member,
-                                        const SharedString &field) {
+void MemberData::imageConvertThreadMain(
+    [[maybe_unused]] const SharedString &member,
+    [[maybe_unused]] const SharedString &field) {
+#ifndef WEBCFACE_SERVER
+    logger->error("MemberData::imageConvertThreadMain is not implemented.");
+#else
     int last_image_flag = -1, last_req_flag = -1;
     logger->trace("imageConvertThreadMain started for {}, {}", member.decode(),
                   field.decode());
@@ -271,6 +281,7 @@ void MemberData::imageConvertThreadMain(const SharedString &member,
             break;
         }
     }
+#endif
 }
 
 } // namespace server
