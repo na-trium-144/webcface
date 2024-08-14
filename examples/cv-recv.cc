d600b8887234d907017809ea5d3fe1e5f37d6bf1
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-08-14 14:25:05 +0900

    onchange周り

--- examples/cv-recv.cc
+++ examples/cv-recv.cc
@@ -1,3 +1,4 @@
+#include <cassert>
 #include <webcface/client.h>
 #include <webcface/image.h>
 #include <opencv2/core.hpp>
@@ -5,87 +6,89 @@
 #include <thread>
 #include <iostream>
 
+webcface::Client wcli;
+
+void img_update1(webcface::Image img);
+void img_update2(webcface::Image img);
+void img_update3(webcface::Image img);
+void img_update4(webcface::Image img);
+void img_update5(webcface::Image img);
+
 int main() {
-    webcface::Client wcli;
     wcli.waitConnection();
 
     // image_sendを5回起動して終了すると5通りのリクエストをして表示するサンプル
 
     webcface::Image img = wcli.member("example_image_send").image("sample");
     img.request();
-    img.onChange([](auto) { std::cout << "image updated" << std::endl; });
-    while (true) {
-        wcli.waitSync();
-        // auto mat = wcli.member("example_image_send").image("sample").mat();
-        // NG
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            assert(img_frame.channels() == 3);
-            assert(img_frame.color_mode() == webcface::ImageColorMode::bgr);
-            cv::Mat mat2(img_frame.rows(), img_frame.cols(), CV_8UC3,
-                         img_frame.data().data());
-            cv::imshow("Display window (image_recv)", mat2);
-            cv::waitKey(1); // Wait for a keystroke in the window
-            cv::imwrite("recv_image.png", mat2);
-            break;
-        }
+    img.onChange(img_update1);
+    wcli.loopSync();
+}
+
+void img_update1(webcface::Image img) {
+    std::cout << "image updated" << std::endl;
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        assert(img_frame.channels() == 3);
+        assert(img_frame.color_mode() == webcface::ImageColorMode::bgr);
+        cv::Mat mat2(img_frame.rows(), img_frame.cols(), CV_8UC3,
+                     img_frame.data().data());
+        cv::imshow("Display window (image_recv)", mat2);
+        cv::waitKey(1); // Wait for a keystroke in the window
+        cv::imwrite("recv_image.png", mat2);
+
+        img.request(webcface::sizeWH(300, 300));
+        img.onChange(img_update2);
     }
-    img.request(webcface::sizeWH(300, 300));
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            cv::Mat mat2(img_frame.rows(), img_frame.cols(), CV_8UC3,
-                         img_frame.data().data());
-            assert(img_frame.channels() == 3);
-            assert(img_frame.color_mode() == webcface::ImageColorMode::bgr);
-            assert(img_frame.rows() == 300);
-            assert(img_frame.cols() == 300);
-            cv::imshow("Display window (image_recv, 300x300)", mat2);
-            cv::waitKey(1); // Wait for a keystroke in the window
-            break;
-        }
+}
+void img_update2(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        cv::Mat mat2(img_frame.rows(), img_frame.cols(), CV_8UC3,
+                     img_frame.data().data());
+        assert(img_frame.channels() == 3);
+        assert(img_frame.color_mode() == webcface::ImageColorMode::bgr);
+        assert(img_frame.rows() == 300);
+        assert(img_frame.cols() == 300);
+        cv::imshow("Display window (image_recv, 300x300)", mat2);
+        cv::waitKey(1); // Wait for a keystroke in the window
+
+        img.request(std::nullopt, webcface::ImageColorMode::gray);
+        img.onChange(img_update3);
     }
-    img.request(std::nullopt, webcface::ImageColorMode::gray);
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            assert(img_frame.channels() == 1);
-            assert(img_frame.color_mode() == webcface::ImageColorMode::gray);
-            cv::Mat mat2(img_frame.rows(), img_frame.cols(), CV_8UC1,
-                         img_frame.data().data());
-            cv::imshow("Display window (image_recv, gray)", mat2);
-            cv::waitKey(1); // Wait for a keystroke in the window
-            break;
-        }
-        std::this_thread::yield();
+}
+void img_update3(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        assert(img_frame.channels() == 1);
+        assert(img_frame.color_mode() == webcface::ImageColorMode::gray);
+        cv::Mat mat2(img_frame.rows(), img_frame.cols(), CV_8UC1,
+                     img_frame.data().data());
+        cv::imshow("Display window (image_recv, gray)", mat2);
+        cv::waitKey(1); // Wait for a keystroke in the window
+
+        img.request(std::nullopt, webcface::ImageCompressMode::jpeg, 20);
+        img.onChange(img_update4);
     }
-    img.request(std::nullopt, webcface::ImageCompressMode::jpeg, 20);
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            cv::Mat mat2 = cv::imdecode(img_frame.data(), cv::IMREAD_COLOR);
-            cv::imshow("Display window (image_recv, jpeg)", mat2);
-            cv::waitKey(1); // Wait for a keystroke in the window
-            break;
-        }
+}
+void img_update4(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        cv::Mat mat2 = cv::imdecode(img_frame.data(), cv::IMREAD_COLOR);
+        cv::imshow("Display window (image_recv, jpeg)", mat2);
+        cv::waitKey(1); // Wait for a keystroke in the window
+
+        img.request(std::nullopt, webcface::ImageCompressMode::png, 1);
+        img.onChange(img_update5);
     }
-    img.request(std::nullopt, webcface::ImageCompressMode::png, 1);
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            cv::Mat mat2 = cv::imdecode(img_frame.data(), cv::IMREAD_COLOR);
-            cv::imshow("Display window (image_recv, png)", mat2);
-            cv::waitKey(0); // Wait for a keystroke in the window
-            break;
-        }
+}
+void img_update5(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        cv::Mat mat2 = cv::imdecode(img_frame.data(), cv::IMREAD_COLOR);
+        cv::imshow("Display window (image_recv, png)", mat2);
+        cv::waitKey(0); // Wait for a keystroke in the window
+
+        wcli.close();
     }
 }
--- examples/image-recv.cc
+++ examples/image-recv.cc
@@ -6,98 +6,105 @@
 #include <fstream>
 #include <cassert>
 
+webcface::Client wcli;
+
+void img_update1(webcface::Image img);
+void img_update2(webcface::Image img);
+void img_update3(webcface::Image img);
+void img_update4(webcface::Image img);
+void img_update5(webcface::Image img);
+
 int main() {
     Magick::InitializeMagick(nullptr);
-    webcface::Client wcli;
     wcli.waitConnection();
+
+    // image_sendを5回起動して終了すると5通りのリクエストをして表示するサンプル
 
     webcface::Image img = wcli.member("example_image_send").image("sample");
     img.request();
-    img.onChange([](auto) { std::cout << "image updated" << std::endl; });
-    while (true) {
-        wcli.waitSync();
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            std::cout << "1. Normal" << std::endl;
-            assert(img_frame.channels() == 3);
-            assert(img_frame.color_mode() == webcface::ImageColorMode::rgb);
-            Magick::Image m(img_frame.rows(), img_frame.cols(), "RGB",
-                            Magick::CharPixel, img_frame.data().data());
-            // m.display();
-            m.write("webcface-example-image-recv-1.png");
-            break;
-        }
+    img.onChange(img_update1);
+    wcli.loopSync();
+}
+
+void img_update1(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        std::cout << "1. Normal" << std::endl;
+        assert(img_frame.channels() == 3);
+        assert(img_frame.color_mode() == webcface::ImageColorMode::rgb);
+        Magick::Image m(img_frame.rows(), img_frame.cols(), "RGB",
+                        Magick::CharPixel, img_frame.data().data());
+        // m.display();
+        m.write("webcface-example-image-recv-1.png");
+
+        img.request(webcface::sizeWH(300, 300));
+        img.onChange(img_update2);
     }
-    img.request(webcface::sizeWH(300, 300));
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            std::cout << "2. Resized to 300x300" << std::endl;
-            assert(img_frame.channels() == 3);
-            assert(img_frame.color_mode() == webcface::ImageColorMode::rgb);
-            assert(img_frame.rows() == 300);
-            assert(img_frame.cols() == 300);
-            Magick::Image m(img_frame.rows(), img_frame.cols(), "RGB",
-                            Magick::CharPixel, img_frame.data().data());
-            // m.display();
-            m.write("webcface-example-image-recv-2.png");
-            break;
-        }
+}
+void img_update2(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        std::cout << "2. Resized to 300x300" << std::endl;
+        assert(img_frame.channels() == 3);
+        assert(img_frame.color_mode() == webcface::ImageColorMode::rgb);
+        assert(img_frame.rows() == 300);
+        assert(img_frame.cols() == 300);
+        Magick::Image m(img_frame.rows(), img_frame.cols(), "RGB",
+                        Magick::CharPixel, img_frame.data().data());
+        // m.display();
+        m.write("webcface-example-image-recv-2.png");
+
+        img.request(std::nullopt, webcface::ImageColorMode::gray);
+        img.onChange(img_update3);
     }
-    img.request(std::nullopt, webcface::ImageColorMode::gray);
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            std::cout << "3. Grayscale" << std::endl;
-            assert(img_frame.channels() == 1);
-            assert(img_frame.color_mode() == webcface::ImageColorMode::gray);
-            Magick::Image m(img_frame.rows(), img_frame.cols(), "K",
-                            Magick::CharPixel, img_frame.data().data());
-            m.type(Magick::TrueColorType);
-            m.negate(true);
-            // m.display();
-            m.write("webcface-example-image-recv-3.jpg");
-            break;
-        }
-        wcli.waitSync();
+}
+
+void img_update3(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        std::cout << "3. Grayscale" << std::endl;
+        assert(img_frame.channels() == 1);
+        assert(img_frame.color_mode() == webcface::ImageColorMode::gray);
+        Magick::Image m(img_frame.rows(), img_frame.cols(), "K",
+                        Magick::CharPixel, img_frame.data().data());
+        m.type(Magick::TrueColorType);
+        m.negate(true);
+        // m.display();
+        m.write("webcface-example-image-recv-3.jpg");
+
+        img.request(std::nullopt, webcface::ImageCompressMode::jpeg, 20);
+        img.onChange(img_update4);
     }
-    img.request(std::nullopt, webcface::ImageCompressMode::jpeg, 20);
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            std::cout << "4. JPEG" << std::endl;
-            // Magick::Blob blob(img_frame.data().data(),
-            // img_frame.data().size()); Magick::Image img(blob,
-            // {img_frame.cols(), img_frame.rows()}, "JPEG"); img.display();
-            std::ofstream ofs("webcface-example-image-recv-4.jpg",
-                              std::ios_base::out | std::ios_base::binary);
-            ofs.write(reinterpret_cast<const char *>(img_frame.data().data()),
-                      static_cast<int>(img_frame.data().size()));
-            break;
-        }
+}
+void img_update4(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        std::cout << "4. JPEG" << std::endl;
+        // Magick::Blob blob(img_frame.data().data(),
+        // img_frame.data().size()); Magick::Image img(blob,
+        // {img_frame.cols(), img_frame.rows()}, "JPEG"); img.display();
+        std::ofstream ofs("webcface-example-image-recv-4.jpg",
+                          std::ios_base::out | std::ios_base::binary);
+        ofs.write(reinterpret_cast<const char *>(img_frame.data().data()),
+                  static_cast<int>(img_frame.data().size()));
+
+        img.request(std::nullopt, webcface::ImageCompressMode::png, 1);
+        img.onChange(img_update5);
     }
-    img.request(std::nullopt, webcface::ImageCompressMode::png, 1);
-    while (true) {
-        wcli.waitSync();
-        // auto mat2 = img.mat(); // OK
-        auto img_frame = img.get();
-        if (!img_frame.empty()) {
-            std::cout << "5. PNG" << std::endl;
-            // Magick::Blob blob(img_frame.data().data(),
-            // img_frame.data().size()); Magick::Image img(blob,
-            // {img_frame.cols(), img_frame.rows()}, "PNG"); img.display();
-            std::ofstream ofs("webcface-example-image-recv-5.png",
-                              std::ios_base::out | std::ios_base::binary);
-            ofs.write(reinterpret_cast<const char *>(img_frame.data().data()),
-                      static_cast<int>(img_frame.data().size()));
-            break;
-        }
+}
+void img_update5(webcface::Image img) {
+    auto img_frame = img.get();
+    if (!img_frame.empty()) {
+        std::cout << "5. PNG" << std::endl;
+        // Magick::Blob blob(img_frame.data().data(),
+        // img_frame.data().size()); Magick::Image img(blob,
+        // {img_frame.cols(), img_frame.rows()}, "PNG"); img.display();
+        std::ofstream ofs("webcface-example-image-recv-5.png",
+                          std::ios_base::out | std::ios_base::binary);
+        ofs.write(reinterpret_cast<const char *>(img_frame.data().data()),
+                  static_cast<int>(img_frame.data().size()));
+
+        std::cout << "Images are in current directory." << std::endl;
+        wcli.close();
     }
-    std::cout << "Images are in current directory." << std::endl;
 }
