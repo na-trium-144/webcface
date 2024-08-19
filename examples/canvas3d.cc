047df8b6f76b5ebcfe3c38c7e6500f248f868827
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-08-19 21:11:55 +0900

    mingwでuse_math_defines

--- examples/canvas3d.cc
+++ examples/canvas3d.cc
@@ -1,10 +1,6 @@
-#ifdef _MSC_VER
 #define _USE_MATH_DEFINES
-#endif
 #include <webcface/client.h>
 #include <webcface/canvas3d.h>
-#include <thread>
-#include <iostream>
 #include <chrono>
 #include <cmath>
 
