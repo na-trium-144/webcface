2aded90cccd445d950c21bdc0d28e99a2bb608ea
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2024-08-24 13:51:56 +0900

    install修正

---
+++ scripts/remove_private_requires.py
@@ -0,0 +1,14 @@
+import os
+import glob
+
+prefix = os.environ["MESON_INSTALL_PREFIX"]
+if "DESTDIR" in os.environ and os.environ["DESTDIR"]:
+    prefix = os.path.join(os.environ["DESTDIR"], prefix)
+
+pc = glob.glob(os.path.join(prefix, "**", "webcface.pc"), recursive=True)
+with open(pc[0], "r") as f:
+    pc_data = f.readlines()
+with open(pc[0], "w") as f:
+    for line in pc_data:
+        if not line.startswith("Requires.private"):
+            f.write(line)
