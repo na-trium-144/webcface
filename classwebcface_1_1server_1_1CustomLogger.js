ba065b949766c6db13b9a9ea31893b8c16cb24e6
na-trium-144 <100704180+na-trium-144@users.noreply.github.com>
2025-12-18 08:21:28 +0000

    Deploying to gh-pages from @ na-trium-144/webcface@a800834c35d124fe404b52afbfddba537f20b714 🚀

--- classwebcface_1_1server_1_1CustomLogger.js
+++ classwebcface_1_1server_1_1CustomLogger.js
@@ -1,5 +1,5 @@
 var classwebcface_1_1server_1_1CustomLogger =
 [
     [ "CustomLogger", "classwebcface_1_1server_1_1CustomLogger.html#a4d0aa8c23965118628e59522d3866b45", null ],
-    [ "log", "classwebcface_1_1server_1_1CustomLogger.html#a00cd2f28d09aa4c6be5d7ae83e1dd6b2", null ]
+    [ "log", "classwebcface_1_1server_1_1CustomLogger.html#ae30d07531fdae6b850bae792424f902c", null ]
 ];
